390c00278e2819d0de3476e75c431230036327f4
Maxime Deveaux <dvxam123@gmail.com>
2018-07-14 15:32:54 +0200

    Add random tile generation

--- src/builders/map.builder.ts
+++ src/builders/map.builder.ts
@@ -9,6 +9,10 @@ export class MapBuilderParameters {
 export class MapBuilder {
     constructor(private game: Phaser.Game) { }
 
+    private static readonly TileNames = [
+        'tile-point', 'tile-line'
+    ]
+
     build(params: MapBuilderParameters) {
         let group = this.game.add.group();
 
@@ -17,8 +21,12 @@ export class MapBuilder {
                 if (params.gridSize.X % 2 == 0 || i + 1 < params.gridSize.X / 2 || j % 2 == 0) {
                     var hexagonX = params.tileWidth * i * 1.5 + (params.tileWidth / 4 * 3) * (j % 2);
                     var hexagonY = params.tileHeight * j / 2;
-                    var hexagon = this.game.add.sprite(hexagonX, hexagonY, "tile");
+                    var hexagon = this.game.add.sprite(hexagonX, hexagonY, 'tile-classic');
                     group.add(hexagon);
+                    if (Math.random() > 0.5) {
+                        var hexagon = this.game.add.sprite(hexagonX, hexagonY, MapBuilder.randomTile());
+                        group.add(hexagon);
+                    }
                 }
             }
         }
@@ -27,5 +35,9 @@ export class MapBuilder {
         group.y = 10;
         
         return group;
+    }
+
+    private static randomTile(): string {
+        return this.TileNames[Math.floor(Math.random()*this.TileNames.length)];
     }
 }
--- src/states/preloader.state.ts
+++ src/states/preloader.state.ts
@@ -1,13 +1,17 @@
 import State from './state';
 
-const tile = require('assets/images/HK_utility-solid_014.png');
+const tileClassic = require('assets/images/HK_utility-solid_014.png');
+const tilePoint = require('assets/images/HK_utility-fow_005.png');
+const tileLine = require('assets/images/HK_utility-fow_006.png');
 
 // The state for loading core resources for the game
 export default class PreloaderState extends State {
   preload(): void {
     console.debug('Assets loading started');
 
-    this.game.load.image('tile', tile)
+    this.game.load.image('tile-classic', tileClassic);
+    this.game.load.image('tile-point', tilePoint);
+    this.game.load.image('tile-line', tileLine);
   }
 
   create(): void {
